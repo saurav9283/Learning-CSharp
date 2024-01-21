f3d529069008629b7e6f9f671a055e33faa2c400
Saurav <sauravkumar00108@gmail.com>
2024-01-21 17:52:36 +0530

    add file

---
+++ second/StarPrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StarPrint
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number to print pattern");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j <= i; j ++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
