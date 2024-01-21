dcefb331209c685d57d59e2b09b87404aaeb8722
Saurav <sauravkumar00108@gmail.com>
2024-01-21 22:26:08 +0530

    add some code

---
+++ second/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Operation
+{
+    public class Calculator
+    {
+        public int sum(int x,int y)
+        {
+            return x + y;
+        }
+        public int subt(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int multiply(int x , int y)
+        {
+            return x * y;
+        }
+        public int devide(int x , int y)
+        {
+            return x / y;
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            int x = 10;
+            int y = 20;
+            int summ = cal.sum(x, y);
+            int subb = cal.subt(x, y);
+            int multi = cal.multiply(x, y);
+            int div = cal.devide(x, y);
+            Console.WriteLine($"Sum for x: {x} and y: {y} is {summ}");
+            Console.WriteLine($"Subtract for x: {x} and y: {y} is {subb}");
+            Console.WriteLine($"Multiply for x: {x} and y: {y} is {multi}");
+            Console.WriteLine($"Devide for x: {x} and y: {y} is {div}");
+
+        }
+    }
+}
---
+++ second/CheckNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CheckEvenNumber
+{
+    public class CheckNumber
+    {
+        public string CheckEven(int a)
+        {
+            if(a %2 == 0)
+            {
+                return "Number is Even";
+            }
+            else
+            {
+                return "Number is Odd";
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cal = new CheckNumber();
+
+            Console.WriteLine(cal.CheckEven(6));
+        }
+    }
+}
