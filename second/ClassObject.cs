70a00fcfef8609275512c08453cf45ea1b0c9401
Saurav <sauravkumar00108@gmail.com>
2024-01-21 20:52:01 +0530

    constructor

---
+++ second/ClassObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Classobj
+{
+    public class Car
+    {
+        public string name;
+        public int price;
+        public int year;
+
+        public void Start()
+        {
+            Console.WriteLine("The car is started");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("The car is Stop");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.name = "BMW";
+            c.price = 5000000;
+            c.year = 2020;
+            Console.WriteLine($"Name of car is {c.name},, price {c.price}");
+            c.Start();
+            c.Stop();
+        }
+    }
+}
---
+++ second/Constructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RunningConstructor
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() // this is default constructor;
+        {
+            Console.WriteLine("Constructor is called");
+        }
+
+        public Person(string nameOfThePerson, int nameOfTheAge)
+        {
+            name = nameOfThePerson;
+            age = nameOfTheAge;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name of the person is :{name}  and age is {age}");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            Person p = new Person();
+            p.Display();
+            Person p1 = new Person("Saurav", 22);
+            p1.Display();
+        }
+    }
+}
