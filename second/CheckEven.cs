830a7d210a5787a2fb53455c84913f1bbb8b7e37
Saurav <sauravkumar00108@gmail.com>
2024-02-01 11:41:21 +0530

    add some code

---
+++ second/CheckEven.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EvenCheck
+{
+    public class Checking
+    {
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.Write("Enter any Number :");
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine($"{a} is a Even number");
+                }
+                else
+                {
+                    Console.WriteLine($"{a} is a Odd Number");
+                }
+            }
+        }
+    }
+}
---
+++ second/check the user.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace class1
+{
+    public class _1stclass1
+    {
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.Write("Enter your age:");
+                int x = Convert.ToInt32(Console.ReadLine());
+                if(x > 18)
+                {
+                    Console.WriteLine("yOU ARE ELIGIBLE");
+                }
+                else
+                {
+                    Console.WriteLine("nOT eNLGIBLE");
+                }
+            }
+            
+        }
+    }
+}
