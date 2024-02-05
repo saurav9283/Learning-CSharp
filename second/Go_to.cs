8a5ee3fc836b937c8c49cf71751c0d2a0a8efd42
Saurav <sauravkumar00108@gmail.com>
2024-02-05 10:31:05 +0530

    add some file

---
+++ second/Go_to.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Go_To
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        Start:
+            Console.Write("Enter a number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("This is 1");
+                        goto End;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("This is 2");
+                        goto Start;
+                    }
+                
+            }
+            End:
+            {
+                Console.WriteLine("Here end of te program");
+            }
+
+        }
+    }
+}
+
